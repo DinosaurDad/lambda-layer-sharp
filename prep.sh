7c05ad11297407bf11c0d34a9bf8e0ec56ea9f5c
Matt Palcic <matt.palcic@expedata.net>
2023-06-23 12:32:26 -0400

    Use node18, and updated sharp for bug fixes

--- prep.sh
+++ prep.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
-exit 0
+#echo "Comment out exit if you want to rebuild the image. Docker must be running."
+#exit 0
 
 # https://github.com/mLupine/docker-lambda
 
-IMAGE_NAME=mlupin/docker-lambda:nodejs14.x-build-arm64
+#NODE_VERSION=14.x
+NODE_VERSION=18.x
+
+IMAGE_NAME=mlupin/docker-lambda:nodejs${NODE_VERSION}-build-arm64
 
 # Install dependencies
-docker run --rm -v "$PWD/root":/root -v "$PWD":/var/task mlupin/docker-lambda:nodejs14.x-build-arm64 npm --no-optional --no-audit --progress=false install
+docker run --rm -v "$PWD/root":/root -v "$PWD":/var/task mlupin/docker-lambda:nodejs${NODE_VERSION}-build-arm64 npm --no-optional --no-audit --progress=false install
 
 # Build the layer
-docker run --rm -v "$PWD/root":/root -v "$PWD":/var/task mlupin/docker-lambda:nodejs14.x-build-arm64 node ./node_modules/webpack/bin/webpack.js
+docker run --rm -v "$PWD/root":/root -v "$PWD":/var/task mlupin/docker-lambda:nodejs${NODE_VERSION}-build-arm64 node ./node_modules/webpack/bin/webpack.js
 
 # Perform a smoke-test
-docker run --rm -w /var/task/dist/nodejs -v "$PWD/root":/root -v "$PWD":/var/task:ro,delegated mlupin/docker-lambda:nodejs14.x-build-arm64 node -e "console.log(require('sharp'))"
+docker run --rm -w /var/task/dist/nodejs -v "$PWD/root":/root -v "$PWD":/var/task:ro,delegated mlupin/docker-lambda:nodejs${NODE_VERSION}-build-arm64 node -e "console.log(require('sharp'))"
 
 
 # This will always execute the code in an x86_64 environment (native on x86 computers, emulated on arm64)
-#docker run --rm -v "$PWD":/var/task:ro,delegated mlupin/docker-lambda:nodejs12.x-x86_64 index.handler
+#docker run --rm -v "$PWD":/var/task:ro,delegated mlupin/docker-lambda:nodejs${NODE_VERSION}-x86_64 index.handler
 
 # This will always execute the code in an arm64 environment (native on arm64 computers, emulated on x86)
-#docker run --rm -v "$PWD":/var/task:ro,delegated mlupin/docker-lambda:nodejs12.x-arm64 index.handler
+#docker run --rm -v "$PWD":/var/task:ro,delegated mlupin/docker-lambda:nodejs${NODE_VERSION}-arm64 index.handler
 
 # This will always execute the code in an environment matching the host computer architecture
-#docker run --rm -v "$PWD":/var/task:ro,delegated mlupin/docker-lambda:nodejs12.x index.handler
+#docker run --rm -v "$PWD":/var/task:ro,delegated mlupin/docker-lambda:nodejs${NODE_VERSION} index.handler
 
